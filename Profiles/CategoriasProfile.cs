df6347143272d7963940d569fa5e70d111bae76d
Walter <silvawalteralipio@gmail.com>
2022-11-03 17:20:13 -0300

    add Categorias Profile

---
+++ Profiles/CategoriasProfile.cs
@@ -0,0 +1,21 @@
+using AluraPlayList.Models;
+using AluraPlayList.Data.DTOs.CategoriasDTOs;
+using AutoMapper;
+
+namespace AluraPlayList.Profiles
+{
+  public class CategoriasProfile : Profile
+  {
+    public CategoriasProfile()
+    {
+      CreateMap<CreateCategoriasDto, Categoria>();
+
+      CreateMap<Categoria, ReadCategoriasDto>()
+        .ForMember(categoria => categoria.Videos, opts =>
+          opts.MapFrom(categoria => categoria.Videos
+            .Select(
+              video => new { video.Id, video.Title, video.Url, video.Description }
+            )));
+    }
+  }
+}
