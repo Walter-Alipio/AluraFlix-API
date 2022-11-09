36e7144ad8519973de05af7b64aad19d1f803dcc
Walter <silvawalteralipio@gmail.com>
2022-11-09 15:20:15 -0300

    update migrations

--- Migrations/20221103191049_Video relação Categorias.Designer.cs
+++ Migrations/20221103191049_Video relação Categorias.Designer.cs
@@ -2,6 +2,7 @@
 using AluraPlayList.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AluraPlayList.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221103191049_Video relação Categorias")]
+    partial class VideorelaçãoCategorias
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
