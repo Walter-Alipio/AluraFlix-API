576b44f69927b1d532a9af69121067c8b9b08f2d
Walter <silvawalteralipio@gmail.com>
2023-01-31 19:52:20 -0300

    Refactoring categoria service

---
+++ PlayListAPI/Services/CategoriaService.cs
@@ -0,0 +1,110 @@
+using PlayListAPI.Data;
+using PlayListAPI.Data.DTOs.CategoriasDTOs;
+using PlayListAPI.Models;
+using AutoMapper;
+using FluentResults;
+using PlayListAPI.Data.DTOs.VideosDTOs;
+using PlayListAPI.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using PlayListAPI.Data.DAOs.Interfaces;
+using System.Text.RegularExpressions;
+
+namespace PlayListAPI.Services
+{
+
+
+  public class CategoriaService : ICategoriaService
+  {
+    private IMapper _mapper;
+    private ICategoriaDAO _dao;
+
+    public CategoriaService(IMapper mapper, ICategoriaDAO dao)
+    {
+      _mapper = mapper;
+      _dao = dao;
+    }
+
+    public async Task<ReadCategoriasDto?> AddCategoriaAsync(CreateCategoriasDto categoriaDto)
+    {
+      if (string.IsNullOrEmpty(categoriaDto.Cor) || ColorPatternIsInvalid(categoriaDto.Cor))
+      {
+        return null;
+      }
+
+      Categoria categoria = _mapper.Map<Categoria>(categoriaDto);
+
+      await _dao.AddAsync(categoria);
+
+      return _mapper.Map<ReadCategoriasDto>(categoria);
+    }
+
+    public async Task<ReadCategoriasDto?> ShowCategoriaByIdAsync(int id)
+    {
+      Categoria? categoria = await _dao.GetCategoriaByIdAsync(id);
+
+      if (categoria == null) return null;
+
+      return _mapper.Map<ReadCategoriasDto>(categoria);
+    }
+
+    public async Task<List<ReadVideoDTO>> ShowVideosByCategoriaIdAsync(int id)
+    {
+      List<Video> videoCategoria = await _dao.GetVideosByCategoriaAsync(id);
+
+      if (!videoCategoria.Any()) return new List<ReadVideoDTO>();
+
+      return _mapper.Map<List<ReadVideoDTO>>(videoCategoria);
+    }
+
+    public async Task<List<ReadCategoriasDto>> ShowAllCategoriasAsync()
+    {
+      List<Categoria> categorias = await _dao.GetCategoriasAsync();
+      if (categorias == null) return new List<ReadCategoriasDto>();
+
+      return _mapper.Map<List<ReadCategoriasDto>>(categorias);
+    }
+
+    public async Task<ReadCategoriasDto?> UpdateCategoriaAsync(int id, UpdateCategoriasDtos updateCategoria)
+    {
+      Categoria? categoria = await _dao.GetCategoriaByIdAsync(id);
+      if (categoria == null) return null;
+
+      _mapper.Map(updateCategoria, categoria);
+
+      await _dao.UpdateAsync(categoria);
+
+      return _mapper.Map<ReadCategoriasDto>(categoria);
+    }
+
+    public async Task<Result> DeleteCategoriasAsync(int id)
+    {
+      Categoria? categoria = await _dao.GetCategoriaByIdAsync(id);
+      if (categoria == null) return Result.Fail("Não encontrado");
+
+      await _dao.DeleteAsync(categoria);
+
+      return Result.Ok();
+    }
+
+    public Result? IsColorPatternValid(UpdateCategoriasDtos dtos)
+    {
+      if (string.IsNullOrEmpty(dtos.Cor)) return null;
+
+      if (ColorPatternIsInvalid(dtos.Cor))
+      {
+        return Result.Fail("O campo cor deve seguir o padrão '#xxxxxx'");
+      }
+
+      return Result.Ok();
+    }
+    private bool ColorPatternIsInvalid(string color)
+    {
+      color = color.ToLower();
+
+      string pattern = @"^#?([\da-f]{6})$";
+      if (Regex.IsMatch(color, pattern, RegexOptions.None)) return false;
+
+      return true;
+    }
+  }
+}
