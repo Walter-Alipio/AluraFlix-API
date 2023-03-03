2b45269c702ee1873cf61698af7c72d9788845bf
Walter <silvawalteralipio@gmail.com>
2023-03-03 17:01:19 -0300

    Refactoring: Change Cadastro to Register

--- PlayListAPI/Controllers/RegisterController.cs
+++ PlayListAPI/Controllers/RegisterController.cs
@@ -6,19 +6,19 @@ namespace PlayListAPI.Controllers;
 
 [Route("[controller]")]
 [ApiController]
-public class CadastroController : ControllerBase
+public class RegisterController : ControllerBase
 {
     private RegisterService _registerService;
 
-    public CadastroController(RegisterService registerService)
+    public RegisterController(RegisterService registerService)
     {
         _registerService = registerService;
     }
 
     [HttpPost]
-    public IActionResult RegisterUser(RegisterUserDto registerUser)
+    public async Task<IActionResult> RegisterUser(RegisterUserDto registerUser)
     {
-        bool result = _registerService.RegisterUser(registerUser);
+        bool result = await _registerService.RegisterUser(registerUser);
         if (!result) return StatusCode(500);
 
         return Ok();
