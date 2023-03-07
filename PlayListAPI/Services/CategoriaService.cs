bc2b13fb56c07f91cdf15d800b0d27e6ea9a06cb
Walter <silvawalteralipio@gmail.com>
2023-03-07 17:59:47 -0300

    Refactoring

--- PlayListAPI/Services/CategoriaService.cs
+++ PlayListAPI/Services/CategoriaService.cs
@@ -12,98 +12,100 @@ using PlayListAPI.Repository;
 
 namespace PlayListAPI.Services
 {
-    public class CategoriaService : ICategoriaService
+  public class CategoriaService : ICategoriaService
+  {
+    private IMapper _mapper;
+    private ICategoriaRepository _repository;
+
+    public CategoriaService(IMapper mapper, ICategoriaRepository dao)
     {
-        private IMapper _mapper;
-        private ICategoriaRepository _repository;
-
-        public CategoriaService(IMapper mapper, ICategoriaRepository dao)
-        {
-            _mapper = mapper;
-            _repository = dao;
-        }
-
-        public async Task<ReadCategoriasDto?> AddCategoriaAsync(CreateCategoriasDto categoriaDto)
-        {
-            if (string.IsNullOrEmpty(categoriaDto.Cor) || ColorPatternIsInvalid(categoriaDto.Cor))
-            {
-                return null;
-            }
-
-            Categoria categoria = _mapper.Map<Categoria>(categoriaDto);
-
-            await _repository.AddAsync(categoria);
-
-            return _mapper.Map<ReadCategoriasDto>(categoria);
-        }
-
-        public async Task<ReadCategoriasDto?> ShowCategoriaByIdAsync(int id)
-        {
-            Categoria? categoria = await _repository.GetByIdAsync(id, c => c.Videos);
-
-            if (categoria == null) return null;
-
-            return _mapper.Map<ReadCategoriasDto>(categoria);
-        }
-
-        public async Task<List<ReadVideoDTO>> ShowVideosByCategoriaIdAsync(int id)
-        {
-            List<Video> videoCategoria = await _repository.GetVideosByCategoriaAsync(id);
-
-            if (!videoCategoria.Any()) return new List<ReadVideoDTO>();
-
-            return _mapper.Map<List<ReadVideoDTO>>(videoCategoria);
-        }
-
-        public async Task<List<ReadCategoriasDto>> ShowAllCategoriasAsync()
-        {
-            List<Categoria>? categorias = await _repository.GetAll(c => c.Videos);
-            if (categorias == null) return new List<ReadCategoriasDto>();
-
-            return _mapper.Map<List<ReadCategoriasDto>>(categorias);
-        }
-
-        public async Task<ReadCategoriasDto?> UpdateCategoriaAsync(int id, UpdateCategoriasDtos updateCategoria)
-        {
-            Categoria? categoria = await _repository.GetByIdAsync(id, c => c.Videos);
-            if (categoria == null) return null;
-
-            _mapper.Map(updateCategoria, categoria);
-
-            await _repository.UpdateAsync(categoria);
-
-            return _mapper.Map<ReadCategoriasDto>(categoria);
-        }
-
-        public async Task<Result> DeleteCategoriasAsync(int id)
-        {
-            Categoria? categoria = await _repository.GetByIdAsync(id);
-            if (categoria == null) return Result.Fail("Não encontrado");
-
-            await _repository.Delete(categoria);
-
-            return Result.Ok();
-        }
-
-        public Result? IsColorPatternValid(UpdateCategoriasDtos dtos)
-        {
-            if (string.IsNullOrEmpty(dtos.Cor)) return null;
-
-            if (ColorPatternIsInvalid(dtos.Cor))
-            {
-                return Result.Fail("O campo cor deve seguir o padrão '#xxxxxx'");
-            }
-
-            return Result.Ok();
-        }
-        private bool ColorPatternIsInvalid(string color)
-        {
-            color = color.ToLower();
-
-            string pattern = @"^#?([\da-f]{6})$";
-            if (Regex.IsMatch(color, pattern, RegexOptions.None)) return false;
-
-            return true;
-        }
+      _mapper = mapper;
+      _repository = dao;
     }
+
+    public async Task<ReadCategoriasDto?> AddCategoriaAsync(CreateCategoriasDto categoriaDto)
+    {
+      if (string.IsNullOrEmpty(categoriaDto.Cor) || ColorPatternIsInvalid(categoriaDto.Cor))
+      {
+        return null;
+      }
+
+      Categoria categoria = _mapper.Map<Categoria>(categoriaDto);
+
+      await _repository.AddAsync(categoria);
+
+      return _mapper.Map<ReadCategoriasDto>(categoria);
+    }
+
+    public async Task<ReadCategoriasDto?> ShowCategoriaByIdAsync(int id)
+    {
+      Categoria? categoria = await _repository.GetByIdAsync(id, c => c.Videos);
+
+      if (categoria == null) return null;
+
+      return _mapper.Map<ReadCategoriasDto>(categoria);
+    }
+
+    public async Task<List<ReadVideoDTO>> ShowVideosByCategoriaIdAsync(int id)
+    {
+      List<Video> videoCategoria = await _repository.GetVideosByCategoriaAsync(id);
+
+      if (!videoCategoria.Any()) return new List<ReadVideoDTO>();
+
+      return _mapper.Map<List<ReadVideoDTO>>(videoCategoria);
+    }
+
+    public async Task<List<ReadCategoriasDto>> ShowAllCategoriasAsync()
+    {
+      List<Categoria>? categorias = await _repository.GetAll(c => c.Videos);
+      if (categorias == null) return new List<ReadCategoriasDto>();
+
+      return _mapper.Map<List<ReadCategoriasDto>>(categorias);
+    }
+
+    public async Task<ReadCategoriasDto?> UpdateCategoriaAsync(int id, UpdateCategoriasDtos updateCategoria)
+    {
+      Categoria? categoria = await _repository.GetByIdAsync(id, c => c.Videos);
+      if (categoria == null) return null;
+
+      _mapper.Map(updateCategoria, categoria);
+
+      await _repository.UpdateAsync(categoria);
+
+      return _mapper.Map<ReadCategoriasDto>(categoria);
+    }
+
+    public async Task<Result> DeleteCategoriasAsync(int id)
+    {
+      Categoria? categoria = await _repository.GetByIdAsync(id);
+      if (categoria == null) return Result.Fail("Não encontrado");
+
+      await _repository.Delete(categoria);
+
+      return Result.Ok();
+    }
+
+    public Result? IsColorPatternValid(UpdateCategoriasDtos dtos)
+    {
+      if (string.IsNullOrEmpty(dtos.Cor)) return null;
+
+      if (ColorPatternIsInvalid(dtos.Cor))
+      {
+        return Result.Fail("O campo cor deve seguir o padrão '#xxxxxx'");
+      }
+
+      return Result.Ok();
+    }
+    private bool ColorPatternIsInvalid(string color)
+    {
+      color = color.ToLower();
+
+      string pattern = @"^#?([\da-f]{6})$";
+      Match match = Regex.Match(color, pattern, RegexOptions.IgnoreCase);
+
+      if (match.Success) return true;
+
+      return false;
+    }
+  }
 }
--- PlayListAPI/Services/RegisterService.cs
+++ PlayListAPI/Services/RegisterService.cs
@@ -6,26 +6,25 @@ namespace PlayListAPI.Services;
 
 public class RegisterService
 {
-    private readonly IMapper _mapper;
-    private readonly UserManager<IdentityUser> _userManager;
-    private readonly RoleManager<IdentityRole> _roleManager;
+  private readonly IMapper _mapper;
+  private readonly UserManager<IdentityUser> _userManager;
+  private readonly RoleManager<IdentityRole> _roleManager;
 
-    public RegisterService(IMapper mapper, UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
-    {
-        _mapper = mapper;
-        _userManager = userManager;
-        _roleManager = roleManager;
-    }
+  public RegisterService(IMapper mapper, UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+  {
+    _mapper = mapper;
+    _userManager = userManager;
+    _roleManager = roleManager;
+  }
 
-    public async Task<bool> RegisterUser(RegisterUserDto registerUser)
-    {
-        IdentityUser identityUser = _mapper.Map<IdentityUser>(registerUser);
-        var resultIdentity = await _userManager.CreateAsync(identityUser, registerUser.Password);
-        var createRoleResult = _roleManager.CreateAsync(new IdentityRole("user")).Result;
-        var userRoleResult = _userManager.AddToRoleAsync(identityUser, "user").Result;
+  public async Task<bool> RegisterUser(RegisterUserDto registerUser)
+  {
+    IdentityUser identityUser = _mapper.Map<IdentityUser>(registerUser);
+    var resultIdentity = await _userManager.CreateAsync(identityUser, registerUser.Password);
+    var userRoleResult = await _userManager.AddToRoleAsync(identityUser, "user");
 
-        if (resultIdentity.Succeeded) return true;
+    if (resultIdentity.Succeeded) return true;
 
-        return false;
-    }
+    return false;
+  }
 }
