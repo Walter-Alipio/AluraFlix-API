f08d16a8dcfd4dd184e1b401268d3da55d7c00d0
Walter <silvawalteralipio@gmail.com>
2023-03-11 12:03:21 -0300

    Refactoring: MapOnlyIfChanges now works when source is null and destiny is not

--- PlayListAPI/Profiles/MapOnlyIfChange.cs
+++ PlayListAPI/Profiles/MapOnlyIfChange.cs
@@ -10,8 +10,13 @@ public static class MapOnlyIfChange
           source.Condition((sourceObject, destObject, sourceProperty, destProperty) =>
           {
             if (sourceProperty == null)
-              return (destProperty == null);
-            return !sourceProperty.Equals(destProperty);
+            {
+              // Se a propriedade de origem for nula, ignorar a propriedade de destino
+              return false;
+            }
+            // Se a propriedade de destino for nulável, definir como null, caso contrário, definir um valor padrão seguro
+            return !sourceProperty.Equals(destProperty)
+              && (Nullable.GetUnderlyingType(destProperty.GetType()) != null ? true : destProperty.Equals(default));
           });
         });
 
