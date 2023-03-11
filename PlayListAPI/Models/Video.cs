83b7fd1d90e581f69496b9910d983905bfa8529e
Walter <silvawalteralipio@gmail.com>
2023-03-11 12:05:51 -0300

    Refactoring: Model video have new attribues

--- PlayListAPI/Models/Video.cs
+++ PlayListAPI/Models/Video.cs
@@ -1,14 +1,21 @@
 
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+
 namespace PlayListAPI.Models
 
 {
-    public class Video : IEntity
-    {
-        public int Id { get; set; }
-        public string Title { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty;
-        public string Url { get; set; } = string.Empty;
-        public int CategoriaId { get; set; }
-        public virtual Categoria? Categoria { get; set; }
-    }
+  public class Video : IEntity
+  {
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string Url { get; set; } = string.Empty;
+    public int CategoriaId { get; set; }
+    public virtual Categoria? Categoria { get; set; }
+    public string AuthorId { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; }
+    public DateTime ModifyAt { get; set; }
+    public DateTime DeletedAt { get; set; }
+  }
 }
