faebeeea48e94601c509a35bb66793ceef22d863
Walter <silvawalteralipio@gmail.com>
2023-03-23 12:15:23 -0300

    Check if token is received

---
+++ PlayListAPI/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace PlayListAPI
+{
+  public class ExceptionHandlerMiddleware
+  {
+    private readonly RequestDelegate _next;
+
+    public ExceptionHandlerMiddleware(RequestDelegate next) =>
+        _next = next;
+
+    public async Task Invoke(HttpContext httpContext)
+    {
+      try
+      {
+        httpContext.Request.Headers.TryGetValue("Authorization", out var key);
+        // System.Console.WriteLine(key);
+
+        await _next(httpContext);
+      }
+      catch (Exception exception)
+      {
+        httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        httpContext.Response.ContentType = "application/json";
+      }
+    }
+
+  }
+}
