34f588742e0cf4ec4a7cd36d072bdc6eb305c5f0
Walter <silvawalteralipio@gmail.com>
2023-03-23 12:11:03 -0300

    Refactoring: adjuste paginated videos response

--- PlayListAPI/Data/DTOs/VideosDTOs/VideosPaginated.cs
+++
@@ -1,9 +0,0 @@
-using PlayListAPI.Data.DTOs.VideosDTOs;
-
-public class VideosPaginated
-{
-  public string Total { get; set; }
-  public string? Next { get; set; }
-  public string? Previews { get; set; }
-  public List<ReadVideoDTO> Videos { get; set; }
-}
---
+++ PlayListAPI/Data/DTOs/VideosDTOs/VideosPaginatedViewModel.cs
@@ -0,0 +1,9 @@
+using PlayListAPI.Data.DTOs.VideosDTOs;
+
+public class VideosPaginatedViewModel
+{
+  public string Total { get; set; } = string.Empty;
+  public string Next { get; set; } = string.Empty;
+  public string Previews { get; set; } = string.Empty;
+  public List<ReadVideoDTO>? Videos { get; set; }
+}
